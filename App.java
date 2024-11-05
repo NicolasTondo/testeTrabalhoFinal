3affe8d8672ec37e87c30c6b715b32b60c191a54
NicolasTondo <nicolastondo@gmail.com>
2024-11-04 21:16:39 -0300

    import de pacotes na classe App

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class App {
+
+    
+}
--- Gerenciador.java
+++ Gerenciador.java
@@ -14,16 +14,19 @@ public class Gerenciador {
         this.itensPendentes = new ArrayList<>();
     }
 
+    // cadastro deposito
     public void cadastrarDeposito(Deposito deposito) {
         depositos.add(deposito);
         Collections.sort(depositos, Comparator.comparing(Deposito::getCodigo));
     }
 
+    // cadastro cliente
     public void cadastrarCliente(Cliente cliente) {
         clientes.add(cliente);
         Collections.sort(clientes, Comparator.comparing(Cliente::getCodigo));
     }
 
+    // cadastro item
     public void cadastrarItem(Item item) {
         for (Item i : itensPendentes) {
             if (i.getCodigo().equals(item.getCodigo())) {
@@ -34,9 +37,25 @@ public class Gerenciador {
         itensPendentes.add(item);
     }
 
+    // consultar itens armazenados
     public void consultarItensArmazenados() {
     }
 
+    // alterar situação de um item
     public void alterarSituacaoItem() {
     }
+
+    // carregar dados iniciais de um item
+    public void dadosIniciais(){
+    }
+
+    // organizar itens para armazenamento 
+    public void organizarItens(){
+    }
+
+    // salvar dados em um arquivo e mostrar erro 
+    // implementar na classe App 
+    public void salvarDados(){
+    }
+
 }
