23ac7604cfd3f9dff0a02ebee3a798371c7e64b2
NicolasTondo <nicolastondo@gmail.com>
2024-11-06 19:41:05 -0300

    menu

--- App/App.java
+++ App/App.java
@@ -1,51 +1,123 @@
 package App;
 import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import Entities.Item;
+import Entities.*;
+import Enum.Situacao;
 
 public class App {
     Scanner scanner = new Scanner(System.in);
-
     private Gerenciador gerenciador;
 
-    public App(){
-        this.gerenciador = gerenciador;
+    public App() {
+        this.gerenciador = new Gerenciador();
     }
 
+    // menu para o usuario 
     public void iniciar() {
         int opcao;
         do {
             System.out.println("\nMenu:");
-            System.out.println("1. Adicionar deposito");
-            System.out.println("2. Adicionar cliente ");
+            System.out.println("\n1. Adicionar deposito");
+            System.out.println("2. Adicionar cliente");
             System.out.println("3. Adicionar item");
-            //captura dos objetos em array
             System.out.println("4. Consultar item armazenado");
             System.out.println("5. Alterar situação de um item");
             System.out.println("6. Carregar dados do item");
             System.out.println("7. Salvar dados");
             System.out.println("0. Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.print("\nEscolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();  
 
+            // switch para chamar os metodos 
             switch (opcao) {
-                case 1 -> gerenciador.cadastrarDeposito();
+                case 1 -> cadastrarDeposito();
                 case 2 -> cadastrarCliente();
                 case 3 -> cadastrarItem();
-                case 4 -> consultaItensArmazenado();
+                case 4 -> gerenciador.consultarItensArmazenados();
                 case 5 -> alterarSituacaoItem();
-                case 6 -> dadosIniciais();
-                case 7 -> salvarDados();
-                case 0 -> System.out.println("Saindo...");
+                case 6 -> carregarDados();
+                case 7 -> gerenciador.salvarDados();
+                case 0 -> System.out.println("Saindo");
                 default -> System.out.println("Opção inválida");
             }
-        } while (opcao != 0);*/
-    } 
+        } while (opcao != 0);
+    }
+
+
+    private void cadastrarDeposito() {
+        try {
+            System.out.print("Digite o código do depósito: ");
+            int codigo = scanner.nextInt();
+            scanner.nextLine();  
+            System.out.print("Digite o nome do depósito: ");
+            String nome = scanner.nextLine();
+            System.out.print("Digite a cidade do depósito: ");
+            String cidade = scanner.nextLine();
+            System.out.print("Digite o endereço do depósito: ");
+            String endereco = scanner.nextLine();
+            System.out.print("Digite a capacidade máxima: ");
+            double capacidadeMaxima = scanner.nextDouble();
+            Deposito deposito = new Deposito(codigo, nome, cidade, endereco, capacidadeMaxima);
+            gerenciador.cadastrarDeposito(deposito);
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar depósito: " + e.getMessage());
+            scanner.nextLine();  
+        }
+    }
+
+    private void cadastrarCliente() {
+        try {
+            System.out.print("Digite o código do cliente: ");
+            int codigo = scanner.nextInt();
+            scanner.nextLine();  
+            System.out.print("Digite o nome do cliente: ");
+            String nome = scanner.nextLine();
+            System.out.print("Digite o telefone do cliente: ");
+            String telefone = scanner.nextLine();
+            Cliente cliente = new Cliente(codigo, nome, telefone);
+            gerenciador.cadastrarCliente(cliente);
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar cliente: " + e.getMessage());
+            scanner.nextLine();  
+        }
+    }
+
+    private void cadastrarItem() {
+        try {
+            System.out.print("Digite o código do item: ");
+            String codigo = scanner.nextLine();
+            System.out.print("Digite a descrição do item: ");
+            String descricao = scanner.nextLine();
+            System.out.print("Digite o volume do item: ");
+            double volume = scanner.nextDouble();
+            System.out.print("Digite o valor declarado do item: ");
+            double valorDeclarado = scanner.nextDouble();
+            System.out.print("Digite a situação (PENDENTE, ARMAZENADO, CANCELADO, RETIRADO): ");
+            Situacao situacao = Situacao.valueOf(scanner.next().toUpperCase());
+            // fazer diferente 
+            Categoria categoria = new Duravel("Setor", "Metal");
+            Cliente cliente = gerenciador.getClientes().get(0);
+            Item item = new Item(codigo, descricao, categoria, cliente, volume, valorDeclarado, situacao);
+            gerenciador.cadastrarItem(item);
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar item: " + e.getMessage());
+            scanner.nextLine(); 
+        }
+    }
+
+    private void alterarSituacaoItem() {
+        try {
+            System.out.print("Digite o código do item a alterar: ");
+            String codigo = scanner.nextLine();
+            System.out.print("Digite a nova situação (PENDENTE, ARMAZENADO, CANCELADO, RETIRADO): ");
+            Situacao novaSituacao = Situacao.valueOf(scanner.next().toUpperCase());
+            gerenciador.alterarSituacaoItem(codigo, novaSituacao);
+        } catch (Exception e) {
+            System.out.println("Erro ao alterar situação: " + e.getMessage());
+            scanner.nextLine(); 
+        }
+    }
+
+    private void carregarDados() {
+    }
 }
--- App/Gerenciador.java
+++ App/Gerenciador.java
@@ -1,37 +1,46 @@
 package App;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import Entities.Cliente;
 import Entities.Deposito;
 import Entities.Item;
+import Enum.Situacao;
 
+// listas
 public class Gerenciador {
     private List<Deposito> depositos;
     private List<Cliente> clientes;
     private List<Item> itensPendentes;
 
+    // construtor
     public Gerenciador() {
         this.depositos = new ArrayList<>();
         this.clientes = new ArrayList<>();
         this.itensPendentes = new ArrayList<>();
     }
 
-    // cadastro deposito
+    // getCliente para usar as informações na App depois 
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    // cadastro de um deposito
+    // adiona na lista e ordena com sort
     public void cadastrarDeposito(Deposito deposito) {
         depositos.add(deposito);
         Collections.sort(depositos, Comparator.comparing(Deposito::getCodigo));
     }
 
-    // cadastro cliente
+    // cadastro de um cliente
+    // adiona na lista e ordena com sort
     public void cadastrarCliente(Cliente cliente) {
         clientes.add(cliente);
         Collections.sort(clientes, Comparator.comparing(Cliente::getCodigo));
     }
 
-    // cadastro item
+    // cadastro de um intem
+    // verifica se o código já existe e adiciona na lista se não existir
     public void cadastrarItem(Item item) {
         for (Item i : itensPendentes) {
             if (i.getCodigo().equals(item.getCodigo())) {
@@ -42,31 +51,49 @@ public class Gerenciador {
         itensPendentes.add(item);
     }
 
-    // consultar itens armazenados
+    // consultar os dados dos depositos
     public void consultarItensArmazenados() {
-        System.out.println("Itens armazenados:");
-        for (Item item : itensPendentes) {
-            System.out.println(item);
+        if (itensPendentes.isEmpty()) {
+            System.out.println("Nenhum item armazenado.");
+        } else {
+            System.out.println("Itens armazenados:");
+            for (Item item : itensPendentes) {
+                System.out.println(item);
+            }
         }
-        /*
-         * 
-         */
     }
 
-    // alterar situação de um item
-    public void alterarSituacaoItem() {
+    public void alterarSituacaoItem(String codigo, Situacao novaSituacao) {
+        for (Item item : itensPendentes) {
+            if (item.getCodigo().equals(codigo)) {
+                item.setSituacao(novaSituacao);
+                System.out.println("Situação do item alterada com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Item não encontrado.");
     }
 
-    // carregar dados iniciais de um item
-    public void dadosIniciais(){
+    // salvar as informações que entraram por teclado do usuario em um arquivo chamado "dados.txt"
+    // usei o FileOutputStream para especificar o padrão "UTF-8" para não ter erro nos caracteres
+    public void salvarDados() {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dados.txt"), "UTF-8"))) {
+            for (Deposito deposito : depositos) {
+                writer.write(deposito.toString());
+                writer.newLine();
+            }
+            for (Cliente cliente : clientes) {
+                writer.write(cliente.toString());
+                writer.newLine();
+            }
+            for (Item item : itensPendentes) {
+                writer.write(item.toString());
+                writer.newLine();
+            }
+            System.out.println("Dados salvos com sucesso!");
+        // fiz o tratamento só para IOException, mas podemos colocar mais, se for o caso
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar os dados: " + e.getMessage());
+        }
     }
-
-    // organizar itens para armazenamento 
-    // nao precisa de metodo com interação: sort 
-
-    // salvar dados em um arquivo e mostrar erro 
-    // implementar na classe App 
-    public void salvarDados(){
-    }
-
 }
